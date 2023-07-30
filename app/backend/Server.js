2fc38e72206cf2aa485f886556af84b4a5541e0d
ricardokuhlkamp <ric.kds@hotmail.com>
2023-07-29 23:43:50 -0300

    Add 'app' directory with backend and frontend

---
+++ app/backend/Server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const routes = require("./routes/TaskRoute")
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT | 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// app.get("/", (req, res) => {
+//   res.send("The Brave Coders");
+// });
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}`));
